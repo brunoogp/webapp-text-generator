f5809ba127dc82a9be96a76c660b4a1518d2e2cf
brunoogp <147428822+brunoogp@users.noreply.github.com>
2025-02-07 22:09:28 -0300

    Update firebaseConfig.ts

--- app/components/firebaseConfig.ts
+++ app/components/firebaseConfig.ts
@@ -1,18 +1,17 @@
-// firebaseConfig.ts
-import { initializeApp, getApps, getApp } from "firebase/app";
+import { initializeApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAlQEkdSArdyNYMzKVUBJQs5yRoww55Pmc",
   authDomain: "assistente-de-midias-sociais.firebaseapp.com",
   projectId: "assistente-de-midias-sociais",
-  storageBucket: "assistente-de-midias-sociais.appspot.com", // Corrigido
+  storageBucket: "assistente-de-midias-sociais.firebasestorage.app",
   messagingSenderId: "91269961112",
   appId: "1:91269961112:web:6f44e5fff0105c5f6ac7ed"
 };
 
-// Garante que o Firebase só seja inicializado uma vez
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+// 🔥 Garante que o Firebase só seja inicializado uma vez
+const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-export { auth, app };
+export { auth };
